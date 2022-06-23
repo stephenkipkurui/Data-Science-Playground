9edc13ff21006737f975174138a9c6c5fcc860fa
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-06-23 16:17:40 -0500

    Pandas Review

---
+++ Pandas4Everyone/libraries.py
@@ -0,0 +1,13 @@
+
+import pandas_datareader as pdr
+import datetime
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+import datetime
+
+plt.rcParams["figure.figsize"] = (20, 10)
+sns.set(rc={'figure.figsize':(20,10)})
